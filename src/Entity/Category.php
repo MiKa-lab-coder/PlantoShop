6bfbfbc83f8631b5e21bcff78400f4449d37417a
mika <wursthorn.mika@gmail.com>
2025-10-31 10:47:13 +0100

    feat: add groups

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -6,6 +6,7 @@ use App\Repository\CategoryRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: CategoryRepository::class)]
 class Category
@@ -13,13 +14,16 @@ class Category
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['plant:read', 'order:read', 'cart:read'])] // ID souvent utile dans les relations
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['category:read', 'category:write'])]
     private ?string $name = null; // Nom de la catégorie
 
     // Une catégorie contient des plantes
     #[ORM\OneToMany(mappedBy: 'category', targetEntity: Plant::class, orphanRemoval: true)]
+    #[Groups(['category:read'])]
     private Collection $plants;
 
     public function __construct()
