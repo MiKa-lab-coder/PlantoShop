4f0c5d28ebc340e7934197244259495915c72573
mika <wursthorn.mika@gmail.com>
2025-11-05 10:18:12 +0100

    feat: add search()

--- src/Controller/PlantController.php
+++ src/Controller/PlantController.php
@@ -24,12 +24,24 @@ class PlantController extends AbstractController
         return $this->json($plants, Response::HTTP_OK, [], ['groups' => 'plant:read']);
     }
 
+    #[Route('/api/plants/search/{query}', name: 'api_plants_search', methods: ['GET'])]
+    public function search(string $query, PlantRepository $plantRepository): JsonResponse
+    {
+        $plants = $plantRepository->createQueryBuilder('p')
+            ->where('p.name LIKE :query')
+            ->setParameter('query', '%' . $query . '%')
+            ->getQuery()
+            ->getResult();
+
+        return $this->json($plants, Response::HTTP_OK, [], ['groups' => 'plant:read']);
+    }
+
     #[Route('/api/plants/{id}', name: 'api_plants_show', methods: ['GET'])]
     public function show(Plant $plant): JsonResponse
     {
         return $this->json($plant, Response::HTTP_OK, [], ['groups' => 'plant:read']);
     }
-    
+
     #[Route('/api/plants', name: 'api_plants_create', methods: ['POST'])]
     public function create(
         Request $request,
@@ -43,7 +55,7 @@ class PlantController extends AbstractController
 
         $data = json_decode($request->getContent(), true);
         $plant = $serializer->deserialize($request->getContent(), Plant::class, 'json', ['groups' => 'plant:write']);
-        
+
         if (empty($data['category_id'])) {
             return $this->json(['error' => 'category_id is required'], Response::HTTP_BAD_REQUEST);
         }
@@ -65,13 +77,13 @@ class PlantController extends AbstractController
             }
             return $this->json(['errors' => $errorMessages], Response::HTTP_BAD_REQUEST);
         }
-        
+
         $entityManager->persist($plant);
         $entityManager->flush();
-        
+
         return $this->json($plant, Response::HTTP_CREATED, [], ['groups' => 'plant:read']);
     }
-    
+
     #[Route('/api/plants/{id}', name: 'api_plants_update', methods: ['PUT'])]
     public function update(
         Request $request,
@@ -89,7 +101,7 @@ class PlantController extends AbstractController
 
         $data = json_decode($request->getContent(), true);
         $serializer->deserialize($request->getContent(), Plant::class, 'json', ['object_to_populate' => $plant, 'groups' => 'plant:write']);
-        
+
         // Gérer la mise à jour de la catégorie si elle est fournie
         if (isset($data['category_id'])) {
             $category = $categoryRepository->find($data['category_id']);
@@ -112,7 +124,7 @@ class PlantController extends AbstractController
 
         return $this->json($plant, Response::HTTP_OK, [], ['groups' => 'plant:read']);
     }
-    
+
     #[Route('/api/plants/{id}', name: 'api_plants_delete', methods: ['DELETE'])]
     public function destroy(Plant $plant, EntityManagerInterface $entityManager): JsonResponse
     {
