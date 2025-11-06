8375558022d5407aa100bf6a51d8a204af9b028a
mika <wursthorn.mika@gmail.com>
2025-11-06 16:22:35 +0100

    feat: add get/add/rmPlants()

--- src/Entity/Order.php
+++ src/Entity/Order.php
@@ -3,34 +3,38 @@
 namespace App\Entity;
 
 use App\Repository\OrderRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: OrderRepository::class)]
-#[ORM\Table(name: 'orders')]
+#[ORM\Table(name: '`order`')]
 class Order
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
-    #[Groups(['order:read', 'order:write', 'plant:read', 'cart:read', 'user:read'])]
+    #[Groups(['order:read', 'user:read'])]
     private ?int $id = null;
 
-    // Un client a plusieurs commandes
     #[ORM\ManyToOne(targetEntity: User::class, inversedBy: 'orders')]
     #[ORM\JoinColumn(nullable: false)]
-    #[Groups(['order:read', 'order:write'])]
+    #[Groups(['order:read'])]
     private ?User $client = null;
 
-    // Une commande correspond a un panier
-    #[ORM\OneToOne(inversedBy: 'order', cascade: ['persist', 'remove'])]
-    #[ORM\JoinColumn(nullable: false)]
-    #[Groups(['order:read', 'order:write'])]
-    private ?Cart $cart = null;
-
-    #[ORM\OneToOne(mappedBy: 'orderRef', cascade: ['persist', 'remove'])]
-    #[Groups(['order:read', 'order:write'])]
+    #[ORM\OneToOne(mappedBy: 'theOrder', cascade: ['persist', 'remove'])]
+    #[Groups(['order:read'])]
     private ?OrderDetails $orderDetails = null;
+
+    #[ORM\ManyToMany(targetEntity: Plant::class, inversedBy: 'orders')]
+    #[Groups(['order:read'])]
+    private Collection $plants;
+
+    public function __construct()
+    {
+        $this->plants = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -45,19 +49,6 @@ class Order
     public function setClient(?User $client): static
     {
         $this->client = $client;
-
-        return $this;
-    }
-
-    public function getCart(): ?Cart
-    {
-        return $this->cart;
-    }
-
-    public function setCart(Cart $cart): static
-    {
-        $this->cart = $cart;
-
         return $this;
     }
 
@@ -68,13 +59,32 @@ class Order
 
     public function setOrderDetails(OrderDetails $orderDetails): static
     {
-        // set the owning side of the relation if necessary
-        if ($orderDetails->getOrderRef() !== $this) {
-            $orderDetails->setOrderRef($this);
+        if ($orderDetails->getTheOrder() !== $this) {
+            $orderDetails->setTheOrder($this);
         }
-
         $this->orderDetails = $orderDetails;
+        return $this;
+    }
 
+    /**
+     * @return Collection<int, Plant>
+     */
+    public function getPlants(): Collection
+    {
+        return $this->plants;
+    }
+
+    public function addPlant(Plant $plant): static
+    {
+        if (!$this->plants->contains($plant)) {
+            $this->plants->add($plant);
+        }
+        return $this;
+    }
+
+    public function removePlant(Plant $plant): static
+    {
+        $this->plants->removeElement($plant);
         return $this;
     }
 }
