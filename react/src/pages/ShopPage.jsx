62e062be2eba2f25aa6ee14eb3661ffbc4a7d2b2
mika <wursthorn.mika@gmail.com>
2025-11-06 16:03:32 +0100

    feat :create ShopPage.jsx

---
+++ react/src/pages/ShopPage.jsx
@@ -0,0 +1,15 @@
+function ShopPage ()
+{
+
+
+
+    return(
+        <div className="min-h-[calc(100vh-80px)] w-full flex flex-col items-center justify-center p-4">
+            <div className="bg-white bg-opacity-90 p-8 rounded-lg shadow-lg text-center">
+                <h2 className="text-3xl font-bold text-green-700 mb-4">Shop</h2>
+            </div>
+        </div>
+    );
+}
+
+export default ShopPage;
