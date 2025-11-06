862f1ef554b40418ea74b03ab9e303fbc8b3a080
mika <wursthorn.mika@gmail.com>
2025-11-06 10:28:45 +0100

    fix: fix comment

--- react/src/pages/HomePage.jsx
+++ react/src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import SearchBar from '../components/SearchBar.jsx';
-import wallpaper from '../assets/img/wallpaper.png'; // Importer l'image
+import wallpaper from '../assets/img/wallpaper.png';
 
 function HomePage() {
   const pageStyle = {
@@ -13,6 +13,7 @@ function HomePage() {
       <div className="bg-white bg-opacity-90 p-8 rounded-lg shadow-lg text-center">
         <h2 className="text-3xl font-bold text-green-700 mb-4">Bienvenue sur PlantoShop !</h2>
         <p className="text-slate-600 mb-8">Trouvez la plante parfaite pour votre maison.</p>
+          {/* Composant de recherche */}
         <SearchBar />
       </div>
     </div>
