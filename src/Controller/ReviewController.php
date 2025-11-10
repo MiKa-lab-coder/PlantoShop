fb1533c8b6599bbb28a2ba4bc68226d7e9ae202a
mika <wursthorn.mika@gmail.com>
2025-11-10 14:39:41 +0100

    feat: add ExitingReview

--- src/Controller/ReviewController.php
+++ src/Controller/ReviewController.php
@@ -18,6 +18,7 @@ use Symfony\Component\Validator\Validator\ValidatorInterface;
 #[Route('/api')]
 class ReviewController extends AbstractController
 {
+    // Récuperation des avis d'une plante
     #[Route('/plants/{plantId}/reviews', name: 'api_reviews_list_for_plant', methods: ['GET'])]
     public function listForPlant(int $plantId, ReviewRepository $reviewRepository): JsonResponse
     {
@@ -25,6 +26,18 @@ class ReviewController extends AbstractController
         return $this->json($reviews, Response::HTTP_OK, [], ['groups' => 'review:read']);
     }
 
+    // Récuperation des avis de l'utilisateur
+    #[Route('/user/reviews', name: 'api_user_reviews_list', methods: ['GET'])]
+    public function listUserReviews(ReviewRepository $reviewRepository): JsonResponse
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        $user = $this->getUser();
+        
+        $reviews = $reviewRepository->findBy(['userId' => $user->getId()], ['createdAt' => 'DESC']);
+        return $this->json($reviews, Response::HTTP_OK, [], ['groups' => 'review:read']);
+    }
+
+    // Création d'un avis pour une plante
     #[Route('/plants/{plantId}/reviews', name: 'api_reviews_create_for_plant', methods: ['POST'])]
     public function create(
         int $plantId,
@@ -47,7 +60,6 @@ class ReviewController extends AbstractController
         $hasOrderedPlant = false;
         /** @var Order $order */
         foreach ($user->getOrders() as $order) {
-            // Nouvelle logique de vérification
             if ($order->getPlants()->contains($plant)) {
                 $hasOrderedPlant = true;
                 break;
@@ -58,10 +70,19 @@ class ReviewController extends AbstractController
             return $this->json(['error' => 'Vous ne pouvez laisser un avis que pour les plantes que vous avez achetées.'],
                 Response::HTTP_FORBIDDEN);
         }
+        
+        // Vérification si un avis existe déjà pour cette plante par cet utilisateur
+        $existingReview = $documentManager->getRepository(Review::class)->findOneBy([
+            'userId' => $user->getId(),
+            'plantId' => $plantId
+        ]);
+
+        if ($existingReview) {
+            return $this->json(['error' => 'Vous avez déjà laissé un avis pour cette plante.'], Response::HTTP_CONFLICT);
+        }
 
         /** @var Review $review */
-        $review = $serializer->deserialize($request->getContent(), Review::class, 'json',
-            ['groups' => 'review:write']);
+        $review = $serializer->deserialize($request->getContent(), Review::class, 'json', ['groups' => 'review:write']);
         
         $review->setUserId($user->getId());
         $review->setUsername($user->getFirstName());
