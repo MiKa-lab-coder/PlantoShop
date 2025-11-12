e8f51f35a68f80cd9c4fcfe591d99afaa89f9329
mika <wursthorn.mika@gmail.com>
2025-11-12 13:30:49 +0100

    feat create FileUploader.php

---
+++ src/Service/FileUploader.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+class FileUploader
+{
+    private $targetDirectory;
+    private $slugger;
+
+    public function __construct($targetDirectory, SluggerInterface $slugger)
+    {
+        $this->targetDirectory = $targetDirectory;
+        $this->slugger = $slugger;
+    }
+    // Fonction pour uploader un fichier
+    public function upload(UploadedFile $file): string
+    {
+        $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+        $safeFilename = $this->slugger->slug($originalFilename);
+        $fileName = $safeFilename.'-'.uniqid().'.'.$file->guessExtension();
+
+        try {
+            $file->move($this->getTargetDirectory(), $fileName);
+        } catch (FileException $e) {
+            // Gérer l'exception si quelque chose se passe pendant l'upload
+            throw new \Exception('Failed to upload file: '.$e->getMessage());
+        }
+
+        // On retourne le chemin public du fichier
+        return '/uploads/plant_images/' . $fileName;
+    }
+
+    public function getTargetDirectory(): string
+    {
+        return $this->targetDirectory;
+    }
+}
